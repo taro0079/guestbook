3ae665696b4545b5263ae0b14b7fd28f2fae4a88
Taro Morita <morita0079@gmail.com>
2023-09-24 23:35:27 +0900

    add slug

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -45,8 +45,6 @@ class CommentCrudController extends AbstractCrudController
 
         if (Crud::PAGE_EDIT === $pageName) {
             yield $createdAt->setFormTypeOption('disabled', true);
-        } else {
-            yield $createdAt;
         }
     }
 }
